2fe2ae91e6560c5cf107ce54dcac317e32ff7c10
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2022-06-09 20:58:56 -0300

    update

--- src/controllers/FilmeControllers.js
+++ src/controllers/FilmeControllers.js
@@ -26,9 +26,87 @@ const getById = async (req,res) => {
         res.status(500).send({err: err.message});//vem do objeto erro
     };
 };
+//rota de criação do filme
+const criar = (req,res ) => {
+    try{
+        res.render("criar", {message});
+    }catch(err){//deu erro, venha nesse caminho
+        res.status(500).send({err: err.message});//vem do objeto erro
+    };
+};
+const criacao  = async (req,res) =>{
+    try{
+        const filme = req.body;//a requisição que vem do body, pegando os dados que vem do body
+        if(
+            !filme.nome ||
+            !filme.descricao ||
+            !filme.imagem
+        ){
+            message = "Preencha todos os campos para cadastro!"
+            type = "danger";
+            return res.redirect("/criar");
+        }
+        await Filme.create(filme);//model filme e cria o filme que chegou, async espera essa transação
+        res.redirect("/");
+    }catch(err){//deu erro, venha nesse caminho
+        res.status(500).send({err: err.message});//vem do objeto erro
+    };
+};
+
+//rota editar filme
+const editar1 = async (req,res) => {
+    const filme = await Filme.findByPk(req.params.id);
+
+    if(!filme){
+        res.render("editar", {
+            message: "Filme não foi encontrado!"
+        });
+    }
+    res.render("editar",{
+        filme,
+        message:"",
+    });
+};
+
+//rota de edição do filme
+const editar = async (req,res) => {
+    try{
+        const filme = await Filme.findByPk(req.params.id);
+        const {nome, descricao, imagem } = req.body;
+
+        filme.nome = nome;
+        filme.descricao = descricao;
+        filme.imagem = imagem;
+
+        const filmeEditado = await filme.save();
+        // res.render("editar", {
+        //     filme: filmeEditado,
+        //     message:"Filme editado com sucesso!",
+        // });
+        res.redirect("/");
+    }catch(err){//deu erro, venha nesse caminho
+        res.status(500).send({err: err.message});//vem do objeto erro
+    };
+};
+
+//rota deletar o filme
+const deletar = async (req,res) => {
+    try{
+        await Filme.destroy({where : { id: req.params.id}});
+        message = "Filme removido com sucesso"
+        res.redirect("/");
+    }catch(err){//deu erro, venha nesse caminho
+        res.status(500).send({err: err.message});//vem do objeto erro
+    };
+}
 
 module.exports = {//exportando todas as rotas que a gente usar qui
     getAll,
-    getById
+    getById,
+    criar,
+    criacao,
+    editar1,
+    editar,
+    deletar
 }
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,5 +3,9 @@ const FilmeController = require("../controllers/FilmeControllers");//acessa o co
 
 routes.get("/", FilmeController.getAll);//listar tudo que tem na rota (cadastrado no banco)
 routes.get("/filmes/:id", FilmeController.getById);//rota para pegar o filme por id e estou chamando a rota detalhes
-
+routes.get("/criar", FilmeController.criar);//rota de exibição da criação do filme
+routes.post("/criacao", FilmeController.criacao);//rota de criacao dos filmes
+routes.get("/editar/:id", FilmeController.editar1);//rota de validação se existe o filme para eu editar
+routes.post("/editar/:id", FilmeController.editar);//rota de edição
+routes.get("/deletar/:id", FilmeController.deletar);//rota para deletar o filme
 module.exports = routes;//exporto ela para qualquer caminho que nesse caso é FilmeController
